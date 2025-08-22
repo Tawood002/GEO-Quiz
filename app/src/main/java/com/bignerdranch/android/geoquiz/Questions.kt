2dc7cac68b6e6c05622f17932e9feb3288093b26
Tawood002 <tadas05@outlook.com>
2025-08-22 12:27:01 +1000

    1185919's Quiz mobile app, scoring 9/10 from them give cerita marked by myself

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Questions.kt
@@ -0,0 +1,8 @@
+package com.bignerdranch.android.geoquiz
+
+//class Questions {
+//}
+
+import androidx.annotation.StringRes
+
+data class Question(@StringRes val textResId: Int, val answer: Boolean)
